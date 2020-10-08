44a4dfd2a0af3f4c363d951507098ef165dc8357
Łukasz Piątkowski <piontec@gmail.com>
2020-10-08 12:32:57 +0200

    make dabs script be able to not change ownership of files

--- dabs.sh
+++ dabs.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker run -it --rm -v "$(pwd)":/abs/workdir/ app_build_suite:latest $@
+docker run -it --rm \
+  -e USE_UID=$(id -u ${USER}) \
+  -e USE_GID=$(id -g ${USER}) \
+  -v "$(pwd)":/abs/workdir/ \
+  app_build_suite:latest $@
