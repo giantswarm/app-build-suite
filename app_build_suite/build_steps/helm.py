0b99fac462544df929f583f78fbb7e30be7a4dfc
Gerald Pape <gerald@giantswarm.io>
2020-11-23 16:52:28 +0100

    fix formatting

--- app_build_suite/build_steps/helm.py
+++ app_build_suite/build_steps/helm.py
@@ -336,14 +336,14 @@ class HelmChartBuilder(BuildStep):
                 # compare our expected chart_file_name with the one returned from helm and fail if differs
                 helm_chart_file_name = os.path.basename(full_chart_path)
                 if helm_chart_file_name != context[context_key_chart_file_name]:
-                    raise BuildError(self.name, f"expected chart file name '{helm_chart_file_name}' got '{context[context_key_chart_file_name]}'")
-                    helm_chart_file_name == context[context_key_chart_file_name]
-                ), f"expected chart file name '{helm_chart_file_name}' got '{context[context_key_chart_file_name]}'"
-                if context[context_key_chart_full_path].endswith(full_chart_path
-                ):
-                    raise BuildError(self.name, f"expected '{context[context_key_chart_full_path]}' to end with '{full_chart_path}'")
-                    full_chart_path
-                ), f"expected '{context[context_key_chart_full_path]}' to end with '{full_chart_path}'"
+                    raise BuildError(
+                        self.name,
+                        f"unexpected chart path '{helm_chart_file_name}' != '{context[context_key_chart_file_name]}'",
+                    )
+                if context[context_key_chart_full_path].endswith(full_chart_path):
+                    raise BuildError(
+                        self.name, f"expected '{context[context_key_chart_full_path]}' to end with '{full_chart_path}'"
+                    )
                 context[context_key_chart_full_path] = full_chart_path
                 context[context_key_chart_file_name] = helm_chart_file_name
         if run_res.returncode != 0:
