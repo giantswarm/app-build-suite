a6be1688166fd1dd2bfc49b70db1645d08fe69c6
Łukasz Piątkowski <piontec@gmail.com>
2020-12-01 13:15:23 +0100

    make dabs.sh use host networking for connecting to existing clusters

--- dabs.sh
+++ dabs.sh
@@ -4,4 +4,5 @@ docker run -it --rm \
   -e USE_UID="$(id -u "${USER}")" \
   -e USE_GID="$(id -g "${USER}")" \
   -v "$(pwd)":/abs/workdir/ \
+  --network host \
   quay.io/giantswarm/app-build-suite:latest "$@"
