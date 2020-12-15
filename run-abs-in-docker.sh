8a0d841be2d1d348fa0e082164273113bfa7ab39
Łukasz Piątkowski <piontec@gmail.com>
2020-12-15 09:05:42 +0100

    add 'versions' helper to dabs.sh

--- run-abs-in-docker.sh
+++ run-abs-in-docker.sh
@@ -1,5 +1,25 @@
 #!/bin/bash -e
 
+if [ $# -eq 1 ] && [ "$1" == "versions" ]; then
+  echo "-> python env:"
+  python --version
+  pip --version
+  pipenv --version
+  echo
+  echo "-> kubectl:"
+  kubectl version --client
+  echo
+  echo "-> helm:"
+  helm version
+  echo
+  echo "-> ct:"
+  ct version
+  echo
+  echo "-> apptestctl:"
+  apptestctl version
+  exit 0
+fi
+
 if [ "${USE_UID:-0}" -ne 0 ] && [ "${USE_GID:-0}" -ne 0 ]; then
   groupadd -f -g "$USE_UID" abs
   useradd -g "$USE_GID" -M -l -u "$USE_UID" abs -d "$ABS_DIR" -s /bin/bash || true
