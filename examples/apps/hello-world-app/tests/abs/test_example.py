dc2255ce38c38623b433e0d4436cb58b750ebd98
Lukasz Piatkowski <lukasz@giantswarm.io>
2020-12-22 16:43:22 +0100

    update pytest-helm-charts and examples (#30)
    * update pytest-helm-charts and examples

--- examples/apps/hello-world-app/tests/abs/test_example.py
+++ examples/apps/hello-world-app/tests/abs/test_example.py
@@ -1,4 +1,6 @@
 import pytest
+import pykube
+from pytest_helm_charts.fixtures import Cluster
 
 
 @pytest.mark.functional
@@ -6,8 +8,7 @@ def test_dummy() -> None:
     assert True
 
 
-# @pytest.mark.functional
-# def test_api_working(kube_cluster: Cluster) -> None:
-#     assert kube_cluster.kube_client is not None
-#     assert len(pykube.Node.objects(kube_cluster.kube_client)) >= 1
-#
+@pytest.mark.functional
+def test_api_working(kube_cluster: Cluster) -> None:
+    assert kube_cluster.kube_client is not None
+    assert len(pykube.Node.objects(kube_cluster.kube_client)) >= 1
