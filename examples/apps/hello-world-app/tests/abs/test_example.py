afa16c6818f4330aa3ce8a70d04118a6a7b4aa44
Łukasz Piątkowski <piontec@gmail.com>
2021-01-07 16:43:48 +0100

    changes in progress

--- examples/apps/hello-world-app/tests/abs/test_example.py
+++ examples/apps/hello-world-app/tests/abs/test_example.py
@@ -3,9 +3,10 @@ import pykube
 from pytest_helm_charts.fixtures import Cluster
 
 
-@pytest.mark.functional
-def test_dummy() -> None:
-    assert True
+@pytest.mark.smoke
+def test_we_have_environment(kube_cluster: Cluster) -> None:
+    assert kube_cluster.kube_client is not None
+    assert len(pykube.Node.objects(kube_cluster.kube_client)) >= 1
 
 
 @pytest.mark.functional
