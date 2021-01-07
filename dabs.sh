78fce73f1c5e50ee3f75e44b4b749759cdb8ed64
Gerald Pape <gerald@giantswarm.io>
2021-01-07 14:22:38 +0100

    Post-release version set for v0.1.2

--- dabs.sh
+++ dabs.sh
@@ -5,4 +5,4 @@ docker run -it --rm \
   -e USE_GID="$(id -g "${USER}")" \
   -v "$(pwd)":/abs/workdir/ \
   --network host \
-  quay.io/giantswarm/app-build-suite:v0.1.2 "$@"
+  quay.io/giantswarm/app-build-suite:latest "$@"
