2487009cad1d492a7d6309c5551347b924c67c1b
Łukasz Piątkowski <piontec@gmail.com>
2021-01-13 13:25:41 +0100

    add kind to docker image

--- dabs.sh
+++ dabs.sh
@@ -3,6 +3,8 @@
 docker run -it --rm \
   -e USE_UID="$(id -u "${USER}")" \
   -e USE_GID="$(id -g "${USER}")" \
+  -e DOCKER_GID="$(getent group docker | cut -d: -f3)" \
   -v "$(pwd)":/abs/workdir/ \
+  -v /var/run/docker.sock:/var/run/docker.sock \
   --network host \
   quay.io/giantswarm/app-build-suite:latest "$@"
