ac3a7ca49cac180b4272c11583d15a80aab93378
Gerald Pape <gerald@giantswarm.io>
2021-01-21 12:21:54 +0100

    do not create the app cr again if it exists

--- app_build_suite/build_steps/base_test_runner.py
+++ app_build_suite/build_steps/base_test_runner.py
@@ -312,11 +312,18 @@ class BaseTestRunner(BuildStep, ABC):
             context[context_key_app_cm_cr] = cm
 
         app_obj = AppCR(self._kube_client, app)
-        logger.info(
-            f"Creating App CR for app '{app_name}' to be deployed in namespace '{namespace}' in"
-            f" version '{app_version}'."
-        )
-        app_obj.create()
+        if not app_obj.exists():
+            logger.info(
+                f"Creating App CR for app '{app_name}' to be deployed in namespace '{namespace}' in"
+                f" version '{app_version}'."
+            )
+            app_obj.create()
+        else:
+            logger.info(
+                f"Skipping App CR creation for app '{app_name}' in namespace '{namespace}' in"
+                f" version '{app_version}'."
+            )
+
         self._wait_for_app_to_be_deployed(app_obj)
         context[context_key_app_cr] = app_obj
 
