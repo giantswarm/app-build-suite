a6ab14f9bc7841fef1addcbb847156c382328bd1
Lukasz Piatkowski <lukasz@giantswarm.io>
2021-02-25 11:11:50 +0100

    Add 'compatibleProviders' to schema (#68)
    - adds 'compatibleProviders' list of string to `ct`'s schema validation file
    - introduces e2e test, where the `app_build_suite` module is invoked to build the example app

--- tests/utils/test_files.py
+++ tests/utils/test_files.py
@@ -4,4 +4,4 @@ from app_build_suite.utils.files import get_file_sha256
 
 def test_get_sha256() -> None:
     filename = os.path.join(os.path.dirname(__file__), "..", "build_steps", "res_test_helm", "Chart.yaml")
-    assert get_file_sha256(filename) == "a8d675fa9d137613632782c05c2a68c1a6aa3ca463b612504d4be17dfa86325b"
+    assert get_file_sha256(filename) == "d7d772229b6187fd20906bf4c457b2077a630eacdaa193ac11482096e505c1bc"
