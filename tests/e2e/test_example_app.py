1aad578eb724946b4d7b73621b1016899d309b8e
Łukasz Piątkowski <piontec@gmail.com>
2021-02-25 09:57:39 +0100

    add 'compatibleProviders' to extended 'ct' validation schema file

--- tests/e2e/test_example_app.py
+++ tests/e2e/test_example_app.py
@@ -4,6 +4,6 @@ from app_build_suite.__main__ import main
 def test_build_example_app(monkeypatch):
     monkeypatch.setattr(
         "sys.argv",
-        ["bogus", "-c", "../../examples/apps/hello-world-app", "--destination", "build/", "--skip-steps", "test_all"],
+        ["bogus", "-c", "examples/apps/hello-world-app", "--destination", "build/", "--skip-steps", "test_all"],
     )
     main()
