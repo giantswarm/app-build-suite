89d6fdd55fb042c58bf7c02d3b329ef26ea0bdf2
Łukasz Piątkowski <piontec@gmail.com>
2021-03-02 13:21:05 +0100

    add KubeLinter step

--- app_build_suite/build_steps/helm.py
+++ app_build_suite/build_steps/helm.py
@@ -19,6 +19,7 @@ from app_build_suite.build_steps.build_step import (
     STEP_VALIDATE,
     BuildStepsFilteringPipeline,
     STEP_METADATA,
+    STEP_STATIC_CHECK,
 )
 from app_build_suite.errors import ValidationError, BuildError
 from app_build_suite.types import Context
@@ -246,6 +247,75 @@ class HelmChartToolLinter(BuildStep):
         if run_res.returncode != 0:
             logger.error(f"{self._ct_bin} run failed with exit code {run_res.returncode}")
             raise BuildError(self.name, "Linting failed")
+
+
+class KubeLinter(BuildStep):
+    """
+    Runs kube-linter against the chart.
+    """
+
+    @property
+    def steps_provided(self) -> Set[StepType]:
+        return {STEP_STATIC_CHECK}
+
+    _kubelinter_bin = "kube-linter"
+    _min_kubelinter_version = "0.1.6"
+    _max_kubelinter_version = "0.2.0"
+    _default_kubelinter_cfg_file = ".kube-linter.yaml"
+
+    def initialize_config(self, config_parser: configargparse.ArgParser) -> None:
+        config_parser.add_argument(
+            "--kubelinter-config",
+            required=False,
+            help=f"Path to optional 'kube-linter' config file. By default tries to load "
+            f"'{self._default_kubelinter_cfg_file}'.",
+        )
+
+    def pre_run(self, config: argparse.Namespace) -> None:
+        """
+        Verifies if the required version of `kube-linter` is installed and config options are sane.
+        :param config: the config object
+        :return: None
+        """
+        # verify if binary present
+        self._assert_binary_present_in_path(self._kubelinter_bin)
+        # verify version
+        run_res = run_and_log([self._kubelinter_bin, "version"], capture_output=True)  # nosec
+        version = run_res.stdout.splitlines()[0]
+        self._assert_version_in_range(
+            self._kubelinter_bin, version, self._min_kubelinter_version, self._max_kubelinter_version
+        )
+        # validate config options
+        if config.kubelinter_config is not None and not os.path.isabs(config.kubelinter_config):
+            config.kubelinter_config = os.path.join(os.getcwd(), config.kubelinter_config)
+        if config.kubelinter_config is not None and not os.path.isfile(config.kubelinter_config):
+            raise ValidationError(
+                self.name,
+                f"Kube-linter config file {config.kubelinter_config} doesn't exist.",
+            )
+        _default_cfg_path = os.path.join(config.chart_dir, self._default_kubelinter_cfg_file)
+        if not config.kubelinter_config and os.path.isfile(_default_cfg_path):
+            config.kubelinter_config = _default_cfg_path
+
+    def run(self, config: argparse.Namespace, _: Context) -> None:
+        args = [
+            self._kubelinter_bin,
+            "lint",
+            config.chart_dir,
+        ]
+        if config.debug:
+            args.append("--verbose")
+        if config.kubelinter_config is not None:
+            args.append(f"--config={config.kubelinter_config}")
+        logger.info("Running kube-linter tool")
+        run_res = run_and_log(args, capture_output=True)  # nosec, input params checked above in pre_run
+        for line in run_res.stdout.splitlines():
+            logger.info(line)
+        if run_res.returncode != 0:
+            logger.error(f"{self._kubelinter_bin} run failed with exit code {run_res.returncode}")
+            for line in run_res.stderr.splitlines():
+                logger.error(line)
+            raise BuildError(self.name, "kube-linter failed")
 
 
 class HelmRequirementsUpdater(BuildStep):
@@ -650,6 +720,7 @@ class HelmBuildFilteringPipeline(BuildStepsFilteringPipeline):
                 HelmGitVersionSetter(),
                 HelmRequirementsUpdater(),
                 HelmChartToolLinter(),
+                KubeLinter(),
                 HelmChartMetadataPreparer(),
                 HelmChartBuilder(),
                 HelmChartMetadataFinalizer(),
