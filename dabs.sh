29e438143d90eae5f391a0b8504b40329b86a70d
Gerald Pape <ubergesundheit@users.noreply.github.com>
2021-07-02 15:28:32 +0200

    Remove host network and docker socket mount from dabs script (#110)

--- dabs.sh
+++ dabs.sh
@@ -7,6 +7,4 @@ docker run -it --rm \
   -e USE_GID="$(id -g "${USER}")" \
   -e DOCKER_GID="$(getent group docker | cut -d: -f3)" \
   -v "$(pwd)":/abs/workdir/ \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  --network host \
   "quay.io/giantswarm/app-build-suite:${DABS_TAG}" "$@"
