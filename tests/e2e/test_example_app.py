81c7b02e2b8043c56345e5918bdf44d92896dd2e
Laszlo Uveges <laszlo@giantswarm.io>
2022-10-25 15:33:00 +0200

    Add some tests for the new pipeline and flag

--- tests/e2e/test_example_app.py
+++ tests/e2e/test_example_app.py
@@ -9,3 +9,19 @@ def test_build_example_app(monkeypatch: pytest.MonkeyPatch) -> None:
         ["bogus", "-c", "examples/apps/hello-world-app", "--destination", "build/"],
     )
     main()
+
+
+def test_disable_chart_linter(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        "sys.argv",
+        ["bogus", "-c", "examples/apps/hello-world-app", "--disable-chart-linter", "--destination", "build/"],
+    )
+    main()
+
+
+def test_only_validate_example_app(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        "sys.argv",
+        ["bogus", "-c", "examples/apps/hello-world-app", "-p", "helm-validate-giantswarm"],
+    )
+    main()
