2f3dd32a13fb1cb41babd10cccc37f6d6d053ad6
Łukasz Piątkowski <piontec@gmail.com>
2026-03-18 16:21:25 +0100

    feat: remove '-it' from dabs.sh, as not all build/ci systems allocate a tty and it's not needed

--- dabs.sh
+++ dabs.sh
@@ -2,7 +2,7 @@
 
 DABS_TAG=${DABS_TAG:-"1.7.0"}
 
-docker run -it --rm \
+docker run --rm \
     -e USE_UID="$(id -u "${USER}")" \
     -e USE_GID="$(id -g "${USER}")" \
     -e DOCKER_GID="$(getent group docker | cut -d: -f3)" \
